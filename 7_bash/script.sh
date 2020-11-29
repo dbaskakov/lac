1569522e4df83ffb4f9dd7aea4af5fc6fdc9e3c5
Dmitry Baskakov <dm.baskakov@yandex.ru>
2020-11-29 20:03:46 +0300

    add 7 hw

--- 7_bash/script.sh
+++ 7_bash/script.sh
@@ -1,140 +1,59 @@
 #!/bin/bash
 
-
-написать скрипт для крона
-который раз в час присылает на заданную почту
-- X IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
-- Y запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
-- все ошибки c момента последнего запуска
-- список всех кодов возврата с указанием их кол-ва с момента последнего запуска
-- в письме должно быть прописан обрабатываемый временной диапазон
-- должна быть реализована защита от мультизапуска
-
+#num of IP
 X=5
 
+# num of ADDRESSES
+Y=5
 
-# X IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
-cat access.log | cut -d '-' -f 1 | uniq -c | sort -r | head -n $X
+#SET YOUR EMAIL
+EMAIL=test@yandex.ru
 
-#- Y запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
-cat access.log |  grep -E 'http../' | awk '{ print $11 }' | uniq -c | sort -r
-
-# - все ошибки c момента последнего запуска
-grep -i 404 access.log | wc -l
-
-
-# список всех кодов возврата с указанием их кол-ва с момента последнего запуска
-
-# в письме должно быть прописан обрабатываемый временной диапазон
-date- currentdate
-date- currentdate -60min
-
-# должна быть реализована защита от мультизапуска
-set -o noclobber + rm rf log
-
-
-
-
-
-
-
-#!/bin/bash
- 
-#time
-t=$(date +%d\\/%b\\/%G:$(date --date '-60 min' +%H))
-date0=$(date +%d\ %b\ %G)
-date1=$(date --date '-60 min' +%H\:00)
-date2=$(date +%H\:00)
-#file
-temp=/var/temp.log
+LOG_PATH=/vagrant/access.log
+MAIL_PATH=/tmp/mail.log
 lockfile=/tmp/localfile
-#mail
-mailto=budakov.web@gmail.com
-mailfrom=bash_test@mail.ru
-pass_mailfrom=***
-smtp_serv=smtp.mail.ru:587
 
-
-      ip_select() {
-                awk ' BEGIN {print "Requests:\tAdress:" }{print $1" "$2}'|
-                column -t
-      }
-      
-      code_select(){
-                awk 'BEGIN {print "sum:\tcode:"}{print $1" "$2}'|
-                column -t
-      }
-      
-      srt() {
-        sort|uniq -c|sort -nr
-      }
-   
-   adr() {
-                echo -e "\nadr request"
-                awk -v t=$t '/'$t'/{print $1}' $file 2>/dev/null|
-                srt|
-                head -20|
-                ip_select
-   }
-
-   trg() {
-                echo -e "\ntarget request"
-                awk -v t=$t '/'$t'/ {print $0}' $file 2>/dev/null|
-                awk -F\" '/https/ {print $4}'|
-                srt|
-                head -20|
-                ip_select
-   }
-
-   rtn() {
-                echo -e "\nreturn code:"
-                awk -v t=$t '/'$t'/ {print $9}' $file 2>/dev/null|
-                srt|
-                code_select
-   }
-   
-   err() {
-                echo -e "\nerror_code:"
-                awk -v t=$t '/'$t'/ {print $9}' $file 2>/dev/null|
-                egrep "^4|^5"|
-                srt|
-                code_select
-   }
-
-all(){
-        echo -e "$file\n$date0 $date1 - $date2"
-        adr;trg;rtn;err
+period() {
+   echo "--Current Period--" >> $MAIL_PATH
+   date0=$(date)
+   date1=$(date --date '-60 min')
+   echo $date1 '-' $date0 >> $MAIL_PATH
 }
 
-ml() {
-         all|mail -v -s "Test" -S smtp="$smtp_serv" \
-        -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="$mailfrom" \
-        -S smtp-auth-password="$pass_mailfrom" -S ssl-verify-ignore \
-        -S nss-config-dir=/etc/pki/nssdb -S from=$mailfrom $mailto
+ip() {
+   echo "---Uniq ip---" >> $MAIL_PATH
+   cat $LOG_PATH | awk '{ print $1 }' | sort | uniq -c | sort -nr | head -n $X | column -t >> $MAIL_PATH
+}
+
+addr() {
+   echo "---Uniq ADDR---" >> $MAIL_PATH
+   cat $LOG_PATH | grep -E 'http../' | awk '{ print $11 }' | grep -v '^"-' | sort | uniq -c | sort -nr | head -n $Y | column -t >> $MAIL_PATH
+}
+
+error() {
+   echo "---ERRORS---" >> $MAIL_PATH
+   awk '{print $9}' $LOG_PATH | grep -P '^4|^5' | sort | uniq -c | sort -nr | column -t >> $MAIL_PATH
+
+}
+
+send_mail() {
+   rm $MAIL_PATH || echo 'NO FILE'
+   echo "Send mail!"
+   period
+   ip
+   addr
+   error
+   > $LOG_PATH
+   echo "MAIL_LOG" |  mail -s "Maillog" -a $MAIL_PATH $EMAIL
 }
 
 if ( set -o noclobber; echo "$$" > "$lockfile") 1> /dev/null;
   then
     trap 'rm -f "$lockfile"; exit $?' INT  TERM EXIT
-    ml
-    sleep 30
+    send_mail
+    sleep 3
     rm -f "$lockfile"
     trap - INT TERM exit
   else
    echo "program running"
 fi
-
-```
-file=/root/access.log
-        post(){
-        echo -e "$file\n$date0 $date1 - $date2"
-        echo -e "$(adr)\n$(trg)\n$(rtn)\n$(err)"
-        }
-        main() {
-        if
-        [ -e $temp ]
-        then echo "script running"
-        else touch $temp && mail && rm -f $temp
-        fi
-        }
-```   
